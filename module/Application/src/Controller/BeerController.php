5e12b8ec5ab88ef6b7846d42189e9fd3b4821290
Elton Minetto <eminetto@gmail.com>
2016-09-18 15:57:52 -0300

    Primeira parte da disciplina

---
+++ module/Application/src/Controller/BeerController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class BeerController extends AbstractActionController
+{
+    public $tableGateway;
+
+    public function __construct($tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function indexAction()
+    {
+        $beers = $this->tableGateway->select()->toArray();
+
+        return new ViewModel(['beers' => $beers]);
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id');
+
+        $beer = $this->tableGateway->select(['id' => $id]);
+        if (count($beer) == 0) {
+            throw new \Exception("Beer not found", 404);
+        }
+
+        $this->tableGateway->delete(['id' => $id]);
+
+        // return $this->redirect()->toUrl('/beer');
+        return $this->redirect()->toRoute('beer');
+    }
+}
